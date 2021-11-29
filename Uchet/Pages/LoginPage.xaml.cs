aca12f6bf80ef2dc0a57e94a21a7878011397c19
Олег Енкулёв <senkatelpad@gmail.com>
2021-11-29 21:09:43 +0500

    436ryeyy5

--- Uchet/Pages/LoginPage.xaml.cs
+++ Uchet/Pages/LoginPage.xaml.cs
@@ -26,13 +26,13 @@ namespace Uchet.Pages
 
         private void LoginBTN_Click(object sender, RoutedEventArgs e)
         {
-            if(LoginBox.Text.Length < 4 || PasswordBox.Password.Length < 4)
+            if (LoginBox.Text.Length < 4 || PasswordBox.Password.Length < 4)
             {
                 MessageBox.Show("Не введены данные");
                 return;
             }
 
-            if(Core.DB.Users.Where(s => s.Login == LoginBox.Text && s.Password == PasswordBox.Password).FirstOrDefault() == null)
+            if (Core.DB.Users.Where(s => s.Login.Equals(LoginBox.Text) && s.Password.Equals(PasswordBox.Password)).FirstOrDefault() == null)
             {
                 MessageBox.Show("Данные неверны");
                 return;
@@ -41,6 +41,35 @@ namespace Uchet.Pages
             Core.currentUser = Core.DB.Users.Where(s => s.Login == LoginBox.Text && s.Password == PasswordBox.Password).FirstOrDefault();
 
             Core.OpenUserPage();
+
+            //int count = 96;
+            //Random rnd = new Random();
+
+            //int iCorrect = 0;
+            //for(int i = 0; i < count; i++)
+            //{
+            //    if (Core.DB.Grades.Where(g => g.Id == iCorrect).FirstOrDefault() == null)
+            //    {
+            //        int[] studIds = Core.DB.Users.Where(u => u.Role == 2).Select(u => u.Id).ToArray();
+            //        int currentStudentId = studIds[rnd.Next(0, studIds.Count() - 1)];
+            //        Users currentStudent = Core.DB.Users.Where(u => u.Id == currentStudentId).FirstOrDefault();
+
+            //        int[] subIds = Core.DB.Subjects.Where(s => s.GroupId == currentStudent.GroupID).Select(u => u.Id).ToArray();
+            //        int currentSubjectId = subIds[rnd.Next(0, subIds.Count() - 1)];
+            //        Subjects currentSubject = Core.DB.Subjects.Where(s => s.Id == currentSubjectId).FirstOrDefault();
+
+            //        Grades grade = new Grades { Id = iCorrect,  Grade = rnd.Next(2, 5), Date = DateTime.Today, StudentId = currentStudentId, SubjectID = currentSubjectId, UserId = currentSubject.TeacherId};
+
+            //        Core.DB.Grades.Add(grade);
+            //        Core.DB.SaveChanges();
+            //        MessageBox.Show(count.ToString());
+            //    }
+            //    else
+            //        i--;
+            //    iCorrect++;
+            //}
+
+            //MessageBox.Show("Готово!");
         }
     }
 }
